c8fb41bfc8fb3af47f730baba7bdbd3beb9ceea8
premiumcentraal-boop <premiumcentraal@gmail.com>
2026-08-18 21:45:19 +0200

    mobile v2.5: launch Cyclone V2.5 App Learner shell

--- apps/mobile/app/src/main/java/com/cyclone/mobile/MainActivity.kt
+++ apps/mobile/app/src/main/java/com/cyclone/mobile/MainActivity.kt
@@ -3,22 +3,25 @@ package com.cyclone.mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.cyclone.mobile.applearner.AppLearnerRuntime
 import com.cyclone.mobile.automation.AutomationRuntime
-import com.cyclone.mobile.ui.CycloneMobileV24App
+import com.cyclone.mobile.ui.CycloneMobileV25App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AutomationRuntime.initialize(this)
+        AppLearnerRuntime.initialize(this)
         BridgeClient.start(this)
         setContent {
-            CycloneMobileV24App()
+            CycloneMobileV25App()
         }
     }
 
     override fun onResume() {
         super.onResume()
         AutomationRuntime.initialize(this)
+        AppLearnerRuntime.initialize(this)
         BridgeClient.start(this)
     }
 }
