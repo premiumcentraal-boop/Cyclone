212a8df7cf8691e346f4fa581c3a357c0a7e1365
premiumcentraal-boop <premiumcentraal@gmail.com>
2026-08-18 21:45:59 +0200

    mobile v2.5: bump Android app to 0.7.0-v2.5-beta

--- apps/mobile/app/build.gradle.kts
+++ apps/mobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.cyclone.mobile"
         minSdk = 34
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.6.0-v2.4"
+        versionCode = 8
+        versionName = "0.7.0-v2.5-beta"
     }
 
     buildTypes {
